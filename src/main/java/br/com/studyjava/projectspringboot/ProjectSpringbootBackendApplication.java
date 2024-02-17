183780cee481b40fa94fceeca31ca4d20cb2a7c3
Mateus Brito <135028665+MateusBrito1@users.noreply.github.com>
2024-02-16 23:36:09 -0300

    Initial commit

---
+++ src/main/java/br/com/studyjava/projectspringboot/ProjectSpringbootBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.studyjava.projectspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringbootBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectSpringbootBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/studyjava/projectspringboot/ProjectSpringbootBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studyjava.projectspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringbootBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
